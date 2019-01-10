adb81fccea3a744b690787bf8e00262435b58076
oscar.gay <oscar.gaytan@wizeline.com>
2019-01-10 11:36:04 +0700

    Update files and test

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var newman = require('newman');
 
 newman.run({
-    collection: './collection/backtest.json',
-    environment: './environment/environment.json',
-    globals: './globals/qaAcademy.postman_globals.json',
+    collection: './collection/qaAcademy_test.postman_collection.json',
+    environment: './environment/qaAcademy.postman_environment.json',
+    // globals: './globals/qaAcademy.postman_globals.json',
     reporters: ['cli', 'html']
 
 
